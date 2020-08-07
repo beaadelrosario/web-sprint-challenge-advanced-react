ae00e38a56401e07c055500f0d199dbc21a46ec4
Bea DelRosario <bdelronyc@Beas-MacBook-Pro.local>
2020-08-07 10:00:42 -0700

    removed local storage, realize problem with handleChanges

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useForm } from '../hooks/useForm'
+import React, {useState} from "react";
+import useForm from '../hooks/useForm'
 
 const initialValue = {
   firstName: "",
@@ -16,14 +16,14 @@ const initialValue = {
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  const [values, handleChanges] = useForm(initialValue);
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  // const handleChanges = (event) => {
+  //   setValues({ ...values, [event.target.name]: event.target.value });
+  // };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
     setShowSuccessMessage(true);
   };
 
